ad0dec797e3f0d1f9e20bf64a22563c649c61da9
JayapradhaR <34539218+JayapradhaR@users.noreply.github.com>
2018-06-26 21:17:19 +0530

    Create max.java

---
+++ max.java
@@ -0,0 +1,14 @@
+import java.util.*;
+import java.util.Collections;
+public class max 
+{
+    public static void main(String args[]) 
+    {
+        ArrayList a=new ArrayList();
+        a.add(10);
+        a.add(35);
+        a.add(45);
+        a.add(2);
+        System.out.println(Collections.max(a));
+   }
+}
